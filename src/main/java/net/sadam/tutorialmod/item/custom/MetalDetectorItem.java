a673f2fbb9bdb4609932761ee9faebf6c2a5e542
sadam2006 <businessmansupreme@gmail.com>
2023-08-02 14:39:56 -0600

    Improved Metal Detector Item texture and added 6 different ore modes.

--- src/main/java/net/sadam/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/sadam/tutorialmod/item/custom/MetalDetectorItem.java
@@ -3,24 +3,57 @@ package net.sadam.tutorialmod.item.custom;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 
+import java.util.ArrayList;
+
 public class MetalDetectorItem extends Item {
+
+    private ArrayList<Block> mode = new ArrayList<Block>();
+    private int currentMode = 0;
 
     public MetalDetectorItem(Properties pProperties) {
         super(pProperties);
+        mode.add(Blocks.COAL_ORE);
+        mode.add(Blocks.IRON_ORE);
+        mode.add(Blocks.COPPER_ORE);
+        mode.add(Blocks.GOLD_ORE);
+        mode.add(Blocks.DIAMOND_ORE);
+        mode.add(Blocks.REDSTONE_ORE);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+
+        if(!pLevel.isClientSide) {
+
+            if (currentMode == mode.size() - 1) {
+                currentMode = 0;
+            } else {
+                currentMode++;
+            }
+
+            pPlayer.sendSystemMessage(Component.literal("Mode set to: " +
+                    I18n.get(mode.get(currentMode).getDescriptionId())));
+        }
+
+        return InteractionResultHolder.pass(pPlayer.getItemInHand(pUsedHand));
     }
 
     @Override
     public InteractionResult useOn(UseOnContext pContext) {
 
-        if(pContext.getLevel().isClientSide) {
+        if(!pContext.getLevel().isClientSide) {
             BlockPos positionClicked = pContext.getClickedPos();
             Player player = pContext.getPlayer();
             boolean foundBlock = false;
@@ -37,7 +70,7 @@ public class MetalDetectorItem extends Item {
             }
 
             if(!foundBlock) {
-                player.sendSystemMessage(Component.literal("No Valuables Found."));
+                player.sendSystemMessage(Component.literal("No Valuables Found"));
             }
         }
 
@@ -53,6 +86,6 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.is(Blocks.IRON_ORE) || state.is(Blocks.DIAMOND_ORE);
+        return state.is(mode.get(currentMode));
     }
 }
