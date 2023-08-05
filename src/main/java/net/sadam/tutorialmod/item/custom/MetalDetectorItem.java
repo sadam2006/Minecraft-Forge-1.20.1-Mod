c155860375b8865c272354d89eef75cd3829089d
sadam2006 <businessmansupreme@gmail.com>
2023-08-05 09:58:09 -0600

    Added tooltips for the Sound Block and Metal Detector Item.

--- src/main/java/net/sadam/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/sadam/tutorialmod/item/custom/MetalDetectorItem.java
@@ -9,14 +9,17 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.sadam.tutorialmod.block.ModBlocks;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
 
@@ -99,6 +102,12 @@ public class MetalDetectorItem extends Item {
                 player -> player.broadcastBreakEvent(player.getUsedItemHand()));
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.tutorialmod.metal_detector.tooltip"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 
     private void outputValuableCoordinates(BlockPos blockPos, Player player, Block block) {
