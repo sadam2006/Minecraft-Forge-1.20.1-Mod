a63478342b8f872c994bff46b8bdff1c5430fd6b
sadam2006 <businessmansupreme@gmail.com>
2023-08-12 22:56:48 -0600

    Successfully implemented Data Generation.

---
+++ src/main/java/net/sadam/tutorialmod/datagen/ModItemModelProvider.java
@@ -0,0 +1,36 @@
+package net.sadam.tutorialmod.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+import net.sadam.tutorialmod.TutorialMod;
+import net.sadam.tutorialmod.item.ModItems;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, TutorialMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        simpleItem(ModItems.SAPPHIRE);
+        simpleItem(ModItems.RAW_SAPPHIRE);
+
+        simpleItem(ModItems.METAL_DETECTOR);
+        simpleItem(ModItems.PINE_CONE);
+        simpleItem(ModItems.STRAWBERRY);
+
+        simpleItem(ModItems.CHICK_FIL_A_CHICKEN);
+        simpleItem(ModItems.CHICK_FIL_A_MENU);
+    }
+
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(TutorialMod.MOD_ID, "item/" + item.getId().getPath()));
+    }
+}
